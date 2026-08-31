245bb65bb1872755e63bd2a5296eb27fc38af927
marcelo.molina@uamv.edu.ni <marcelo.molina@uamv.edu.ni>
2026-08-31 10:35:01 -0600

    Actualizacion

--- exepciones/Calculadora_edad.py
+++ exepciones/Calculadora_edad.py
@@ -1,5 +1,12 @@
-try:
-    edad = int(input("Edad: "))
-    print("Edad registrada:", edad)
-except ValueError:
-    print("Ingresa un valor numérico")
+import subprocess
+from colorama import Fore, Style
+while True:
+    try:
+        subprocess.run("cls", shell=True)
+        edad = int(input("Edad: "))
+        break
+    except ValueError:
+        print(Fore.RED + "Ingresa un valor numérico" + Style.RESET_ALL)
+        subprocess.run("pause", shell=True)
+
+print("Edad registrada: ", edad)
